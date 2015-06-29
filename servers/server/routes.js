9f60f71de53f52cc3104e75c8725c46ae1b6bf5d
Sergio Cruz <hello@sergiocruz.me>
2015-06-29 17:17:53 -0400

    Removed stubbed dep

--- servers/server/routes.js
+++ servers/server/routes.js
@@ -11,8 +11,7 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/theatres', require('./api/theatre'));
   app.use('/api/movies', require('./api/movie'));
-  app.use('/api/things', require('./api/thing'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
