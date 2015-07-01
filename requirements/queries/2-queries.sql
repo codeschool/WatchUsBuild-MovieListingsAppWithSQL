de66f991da025aac403a06613126f6e5a1321bd1
Sergio Cruz <hello@sergiocruz.me>
2015-07-01 13:35:49 -0400

    Broke down SQL queries

--- requirements/queries/2-queries.sql
+++ requirements/queries/2-queries.sql
@@ -1,4 +1,14 @@
--- 1) List of movies, with count of theatres
+-- 1
+-- a) List of movies
+SELECT id, title
+FROM movies;
+
+-- b) How many theatres playing a movie (remember DISTINCT from Try SQL?)
+SELECT COUNT(DISTINCT theatre_id)
+FROM showtimes
+WHERE movie_id = 1;
+
+-- c) Can we merge both queries into one? (we don't have to)
 SELECT m.id, m.title, (
   SELECT COUNT(DISTINCT theatre_id)
   FROM showtimes
@@ -7,9 +17,27 @@ SELECT m.id, m.title, (
 FROM movies m;
 
 -- 2) One movie
-SELECT m.id, m.title FROM movies m WHERE id = 1;
+SELECT id, title
+FROM movies
+WHERE id = 1;
 
--- 3) List of theatres playing a movie
+-- 3
+-- a) Theatres playing a movie
+SELECT t.id, t.name
+FROM theatres t
+INNER JOIN showtimes s
+ON s.theatre_id = t.id
+WHERE s.movie_id = 1
+GROUP BY t.id;
+
+-- b) How many showtimes for a movie in a theater?
+SELECT COUNT(*)
+FROM showtimes
+WHERE theatre_id = 1
+AND movie_id = 2
+
+
+-- c) Can we merge the two?
 SELECT t.id, t.name, (
   SELECT COUNT(*)
   FROM showtimes
@@ -23,11 +51,13 @@ WHERE s.movie_id = 1
 GROUP BY t.id;
 
 -- 4) One theatre
-SELECT t.id, t.name FROM theatres t WHERE id = 1;
+SELECT id, name
+FROM theatres
+WHERE id = 1;
 
 -- 5) List of showtimes for a movie and theatre
-SELECT s.time
-FROM showtimes s
+SELECT time
+FROM showtimes
 WHERE movie_id = 1
 AND theatre_id = 2;
 
