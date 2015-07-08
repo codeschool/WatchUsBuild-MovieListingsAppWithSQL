eda8007d478438ff6a6fec50e6e649816a52e0ce
Sergio Cruz <hello@sergiocruz.me>
2015-07-07 23:50:43 -0400

    Updated list of queries ran throughout Soup to Bits video

--- requirements/queries/2-queries.sql
+++ requirements/queries/2-queries.sql
@@ -1,80 +1,76 @@
--- 1
--- a) List of movies
-SELECT id, title
-FROM movies;
+-- THEATRES SECTION
+--
+--
+-- ALL THEATRES:
+--
+-- 1) all theatres
+SELECT id, name
+FROM theatres;
 
--- b) How many theatres playing a movie (remember DISTINCT from Try SQL?)
-SELECT COUNT(DISTINCT theatre_id)
-FROM showtimes
-WHERE movie_id = 1;
+-- THEATRE PAGE:
+--
+-- 2) one theatre
+SELECT id, name
+FROM theatres
+WHERE id = 1;
 
--- c) Can we merge both queries into one? (we don't have to)
-SELECT m.id, m.title, (
-  SELECT COUNT(DISTINCT theatre_id)
-  FROM showtimes
-  WHERE movie_id = m.id
-) as theatresCount
-FROM movies m;
+-- 3) all movies playing in a theatre
+SELECT s.time, m.title
+FROM showtimes s
+INNER JOIN movies m ON m.id = s.movie_id
+WHERE s.theatre_id = 1
+ORDER BY m.title
 
--- 2) One movie
+
+
+-- MOVIES SECTION
+--
+--
+-- MOVIE PAGE:
+--
+-- 4) movie title
 SELECT id, title
 FROM movies
 WHERE id = 1;
 
--- 3
--- a) Theatres playing a movie
-SELECT t.id, t.name
-FROM theatres t
-INNER JOIN showtimes s
-ON s.theatre_id = t.id
-WHERE s.movie_id = 1
-GROUP BY t.id;
 
--- b) How many showtimes for a movie in a theater?
-SELECT COUNT(*)
-FROM showtimes
-WHERE theatre_id = 1
-AND movie_id = 2
-
-
--- c) Can we merge the two?
-SELECT t.id, t.name, (
-  SELECT COUNT(*)
-  FROM showtimes
-  WHERE theatre_id = t.id
-  AND movie_id = s.movie_id
-) as showtimeCount
-FROM theatres t
-INNER JOIN showtimes s
-ON s.theatre_id = t.id
-WHERE s.movie_id = 1
-GROUP BY t.id;
-
--- 4) One theatre
-SELECT id, name
-FROM theatres
-WHERE id = 1;
-
--- 5) List of showtimes for a movie and theatre
-SELECT time
-FROM showtimes
-WHERE movie_id = 1
-AND theatre_id = 2;
-
-
--- 6) List of theatres
-SELECT id, name
-FROM theatres
-
--- 7) List of movies playing in a theatre
-SELECT m.id, m.title
+-- 5) list of theatres and showtimes
+SELECT t.name, s.time
 FROM showtimes s
-INNER JOIN movies m ON s.movie_id = m.id
-WHERE s.theatre_id = 1
+INNER JOIN theatres t ON t.id = s.theatre_id
+WHERE s.movie_id = 1
+ORDER BY t.name;
+
+--
+-- ALL MOVIES PAGE:
+--
+-- 6) all movies
+SELECT id, title
+FROM movies;
+
+-- 7a) counting in how many theatres a movie is being played
+-- point out count is incorrect (look back at data)
+SELECT movie_id, COUNT(theatre_id)
+FROM showtimes
+GROUP BY movie_id;
+
+-- 7b) add distinct to previous query
+SELECT movie_id, COUNT(DISTINCT theatre_id)
+FROM showtimes
+GROUP BY movie_id
+
+
+-- 8a) merge queries #6 and #7 into one
+-- all movie with their titles and count of theatres
+SELECT m.id, m.title, COUNT(DISTINCT s.theatre_id) as theatresCount
+FROM showtimes s
+INNER JOIN movies m ON m.id = s.movie_id
 GROUP BY s.movie_id
 
--- 8) List of showtimes playing in a
-SELECT time
-FROM showtimes
-WHERE theatre_id = 1
-AND movie_id = 2
+-- 8b) show all movies with their count
+--  point that we were missing one movie that didn't have a count
+SELECT m.id, m.title, COUNT(DISTINCT s.theatre_id) as theatresCount
+FROM movies m
+LEFT OUTER JOIN showtimes s ON m.id = s.movie_id
+GROUP BY s.movie_id
+ORDER BY theatresCount DESC
